3cd67f09f7693b956256acfd3624b9caacb35185
Android Sala 15 <android@cuc.edu.co>
2019-06-25 18:55:30 -0500

    Update app.js and add.html

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ function saveTask(){
     var title = document.getElementById('inputTitle').value;
     var date = document.getElementById('inputDate').value;
     var dscp = document.getElementById('inputDscp').value;
-    var task = {name: title, limit:date, dscp:dscp};
-    if(title.lenght !=0 || date.lenght !=0 || dscp.lenght!=0){
+    if(title == null || title.length == 0 ||date == null || date.length == 0 || dscp == null || dscp.length == 0){
+    console.log('Error, There are empty fields');
+    }else{
+        var task = {name: title, limit:date, dscp:dscp};
         tasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
     }
-    
 }
-saveTask("aaa", "12121", "xxdddddddddddddd");
-console.log(tasks);
+function getData(){
+    if(tasks!=null){
+        var showData  = JSON.parse(localStorage.getItem("tasks"));
+        console.log(showData);
+    }else{
+        console.log("There is not tasks");
+    }
+}
+getData();
+
