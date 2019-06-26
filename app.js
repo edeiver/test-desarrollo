ffff9a3246f87eea92fc983747bbaa0bd4b78a22
Android Sala 15 <android@cuc.edu.co>
2019-06-25 19:45:20 -0500

    ultimo

--- app.js
+++ app.js
@@ -1,23 +1,38 @@
-var tasks= [];
-function saveTask(){
-    var title = document.getElementById('inputTitle').value;
-    var date = document.getElementById('inputDate').value;
-    var dscp = document.getElementById('inputDscp').value;
-    if(title == null || title.length == 0 ||date == null || date.length == 0 || dscp == null || dscp.length == 0){
-    console.log('Error, There are empty fields');
-    }else{
-        var task = {name: title, limit:date, dscp:dscp};
-        tasks.push(task);
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-    }
-}
-function getData(){
-    if(tasks!=null){
-        var showData  = JSON.parse(localStorage.getItem("tasks"));
-        console.log(showData);
-    }else{
-        console.log("There is not tasks");
-    }
-}
-getData();
 
+var tasks= [];
+var title = document.getElementById('inputTitle');
+var date = document.getElementById('inputDate');
+var dscp = document.getElementById('inputDscp');
+
+
+function saveTask(){
+
+    if(isEmpty()){
+      alert('Error, There are empty fields');
+    } else {
+        var task = {name: title.value, limit:date.value, dscp:dscp.value};
+        tasks.push(task);
+        localStorage.tasks = JSON.stringify(tasks);
+
+        window.location.href = "index.html";
+
+    }
+
+}
+
+function ListData(){
+    if(localStorage.tasks!=null){
+        var tasks  = JSON.parse(localStorage.getItem("tasks"));
+
+        for (var i = 0; i < tasks.length; i++) {
+          $("#TableBody").append('<tr> <th scope="row"><i class="material-icons">check_circle_outline</i></th> <td class="tdColorBlue"> '+ tasks[i].name +' </td> <td> '+tasks[i].limit+' </td><td> <i class="material-icons iDark">edit</i><i class="material-icons">delete_outline</i></td></tr>'  )
+        }
+
+    }else{
+        alert("There is not tasks");
+    }
+}
+
+function isEmpty(){
+  return title.value == null || title.value.length == 0 ||date.value == null || date.value.length == 0;
+}
