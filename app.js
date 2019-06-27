df2f54696809c43fb884175895afa8643ca8374b
Android Sala 15 <android@cuc.edu.co>
2019-06-26 20:13:40 -0500

    Update app.js and add.html

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ function saveTask(){
         var task = {name: title.value, limit:date.value, dscp:dscp.value};
         tasks.push(task);
         localStorage.tasks = JSON.stringify(tasks);
-
         window.location.href = "index.html";
 
     }
@@ -25,7 +24,7 @@ function ListData(){
         var tasks  = JSON.parse(localStorage.getItem("tasks"));
 
         for (var i = 0; i < tasks.length; i++) {
-          $("#TableBody").append('<tr> <th scope="row"><i class="material-icons">check_circle_outline</i></th> <td class="tdColorBlue" onclick="preview('+i+')"> '+ tasks[i].name +' </td> <td> '+tasks[i].limit+' </td><td> <i class="material-icons iDark">edit</i><i class="material-icons">delete_outline</i></td></tr>'  )
+          $("#TableBody").append('<tr> <th scope="row"><i class="material-icons">check_circle_outline</i></th> <td class="tdColorBlue" onclick="preview('+i+')">'+ tasks[i].name +' </td> <td> '+tasks[i].limit+' </td><td> <i class="material-icons iDark" onclick="editTask('+i+')">edit</i><i class="material-icons" onclick="deleteTask('+i+')">delete_outline</i></td></tr>'  )
         }
 
     }else{
@@ -42,4 +41,25 @@ function preview(pos){
   $('#emptyPrev').hide();
   $('#footerPrev').append('<h4>'+ task.name+'</h4> <small>'+task.limit+'</small><p>'+task.dscp+'</p>')
 
+}
+
+function editTask(pos){
+  location.href ="add.html?pos="+pos;
+  
+}
+function deleteTask(pos){
+  var tasks  = JSON.parse(localStorage.getItem("tasks"));
+  tasks = tasks.splice(pos, 1);
+  localStorage.tasks=JSON.stringify(tasks);
+  location.reload(true);
+}
+function loadData(){
+  var params = location.href.split("=");
+  var pos = params.length > 1? params[1]: null;
+if(pos!=null){
+  var tasks  = JSON.parse(localStorage.getItem("tasks"));
+  title.value = tasks[pos].name;
+  date.value =tasks[pos].limit
+  dscp.value = tasl[pos].dscp;
+  } 
 }
